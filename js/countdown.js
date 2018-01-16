8bdddf371570cace68f04ab6f05681888584cc84
Jonas Salazar <jun.slazer@gmail.com>
2018-01-16 12:58:51 +0800

    Add countdown timer

--- js/countdown.js
+++ js/countdown.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date(October 31 2018 23:59:59 GMT+0800);
 initializeClock('clockdiv', deadline);
